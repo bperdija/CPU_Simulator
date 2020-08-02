7ec30cda5c630f6f73b097764fa98c37c2bd1db3
Boris Perdija <Boris_Perdija@Alfred.hitronhub.home>
2020-08-02 01:29:47 -0700

    Edited Laura's file to add to the Fetch() function. Created a while loop and iterated through to add things to ROB and IQ. Created insert_IQ_instruction() function. Made inherited IQueue in Pipeline. Need to check the IQ for validity and check if instruction is a branch or jump in the Fethc() function. Added cout statements to help guide process of Fetch()

--- main_BORIS.cpp
+++ main_BORIS.cpp
@@ -5,11 +5,24 @@
 #include <vector>
 #include <fstream>
 #include <cstdlib>
-#include <typeinfo> // not needed
+//#include <typeinfo> // not needed
 #include <cmath>
 #include <bitset>
+#include <queue>
 
 using namespace std;
+
+class Memory;
+class Instruction;
+class Pipeline;
+class IQueue;
+class Execute;
+class FetchUnit;
+class RegisterFile;
+class ReorderBuffer;
+class Statistics;
+class HelperFunctions;
+struct ROB_element;
 
 // A class that initializes memory (specify size in an overload constructor); initialize contents -
 // read in csv file and create instruction memory. Note if you read in your csv contents as a string,
@@ -76,15 +89,13 @@ void Memory::read_csv(string file_name)
 class Instruction : public Memory
 {
 public:
-  void uintToBinary(list<unsigned int> given);
+  void uintToBinary(unsigned int given);
 
-  void decode(vector<string> binary);
+  void decode(string binary_code);
   void decode_opcode(string code);
-  void decode_src1(string code);
-  void decode_src2(string code);
-  void decode_dest(string code);
+  int stringToInt(string code);
 
-  vector<string> binary_instructions;
+  string binary_instruction;
   string type;  //bits 0 to 1: used in r, i, p, j
   string opcode;  //bits 2 to 5: used in r, p
   int dest;  //bits 6 to 10: used in r, i, p
@@ -95,43 +106,32 @@ public:
 };
 
 // The uintToBinary() function is used to convert the unsigned int list values from a specified CSV file (That were retrieved using read_csv()) into string type values. These values are then pushed back into the binary_instructions vector inside of the Instruction class.  Precondition: read_csv() must have been called to create a unsinged integer list of the requested CSV file. The parameter of the uintToBinary() function is this unsigned integer list. Postcondition: Converts each unsigned integer element in the original list into a string and then prepends each string into the binary_instructions vector in the Instruction class.
-void Instruction::uintToBinary(list<unsigned int> given)
+void Instruction::uintToBinary(unsigned int given)
 {
-  // Initalize iterator to traverse unsigned integer list.
-	list<unsigned int>::iterator it;
+  // initialize string named binary_instruction that holds the converted string (that was once an unsigned integer).
+	binary_instruction = bitset<32>(given).to_string();
+  cout << "conversion: " << binary_instruction << ", ";
 
-	for (it = given.begin(); it != given.end(); ++it)
-  {
-    // initialize temporary string named temp_string that holds the converted string (that was once an unsigned integer).
-		string temp_string = bitset<32>(*it).to_string();
-    cout << "conversion: " << temp_string << ", ";
-    // Push back each temp_string into the binary_instructions vector.
-		binary_instructions.push_back(temp_string);
-	}
   cout << "Done conversion" << endl;
 }
 
 // The decode() function is used to determine what type of instruction format (R,I,J, or P type) each element in the vector string from the Instruction class is. After that, this function will write to the "type" and "opcode" string variables in the Instruction class, as well as the "dest", "src1", "src2", "immediate" and "address" integer values in the instruction class.  Precondition: A vector string that only contained binary values was successfully passed into the function. Postcondition: Determined what type of instruction format (R,I,J, or P type) each element in the vector string was. Wrote to the "type" and "opcode" string variables in the Instruction class, as well as the "dest", "src1", "src2", "immediate" and "address" integer values in the instruction class.
-void Instruction::decode(vector<string> binary)
+void Instruction::decode(string binary_code)
 {
-	vector<string>::iterator it;
-
-	for (it = binary.begin(); it != binary.end(); ++it)
-	{
-		string type_bits = it->substr(0,2);
+		string type_bits = binary_code.substr(0,2);
     cout << "type_bits: " << type_bits << endl;
 
 		if (type_bits == "00")
 		{//type r
 			type = "r";
-			string opcode_bits = it->substr(2,4);
-      string dest_bits = it->substr(6,5);
-      string src1_bits = it->substr(11,5);
-      string src2_bits = it->substr(16,5);
+			string opcode_bits = binary_code.substr(2,4);
+      string dest_bits = binary_code.substr(6,5);
+      string src1_bits = binary_code.substr(11,5);
+      string src2_bits = binary_code.substr(16,5);
 			decode_opcode(opcode_bits);
-      dest = stoi(dest_bits, 0, 2);
-      src1 = stoi(src1_bits, 0, 2);
-      src2 = stoi(src2_bits, 0, 2);
+      dest = stringToInt(dest_bits);
+      src1 = stringToInt(src1_bits);
+      src2 = stringToInt(src2_bits);
 
       cout << "dest: " <<  dest << " " << "src1: " << src1 << " " << "src2: " << src2 << endl << endl;
       /*
@@ -145,21 +145,23 @@ void Instruction::decode(vector<string> binary)
 		else if (type_bits == "01")
     {
 			type = "i";
-			string opcode_bits = it->substr(2,4);
-      string dest_bits = it->substr(6,5);
-      string src1_bits = it->substr(11,5);
-      string immediate_bits = it->substr(16, 16); // PLEASE CHECK IF THIS IS CORRECT!!!!!!!!!!
+			string opcode_bits = binary_code.substr(2,4);
+      string dest_bits = binary_code.substr(6,5);
+      string src1_bits = binary_code.substr(11,5);
+      string immediate_bits = binary_code.substr(16, 16);
 			decode_opcode(opcode_bits);
-      dest = stoi(dest_bits, 0, 2);
-      src1 = stoi(src1_bits, 0, 2);
+      dest = stringToInt(dest_bits);
+      src1 = stringToInt(src1_bits);
+      immediate = stringToInt(immediate_bits);
       cout << "dest: " <<  dest << " " << "src1: " << src1 << " " << endl << endl;
 		}
 
     // type j
 		else if (type_bits == "10")
     {
-      // string address == it->substr(2,30);
-			type = "j";
+      type = "j";
+      string address_bits = binary_code.substr(2,30);
+      address = stringToInt(address_bits);
 
 		}
 
@@ -167,17 +169,16 @@ void Instruction::decode(vector<string> binary)
 		else if (type_bits == "11")
     {
 			type = "p";
-			string opcode_bits = it->substr(2,4);
-      string dest_bits = it->substr(6,5);
+			string opcode_bits = binary_code.substr(2,4);
+      string dest_bits = binary_code.substr(6,5);
 			decode_opcode(opcode_bits);
-      dest = stoi(dest_bits, 0, 2);
-      cout << "dest: " << dest <<  endl;
+      dest = stringToInt(dest_bits);
+      cout << "dest: " << dest <<  endl << endl;
 		}
 
 		else{
 			cout << "Error: invalid instruction type." << endl << endl;
 		}
-	}
 }
 
 void Instruction::decode_opcode(string code)
@@ -229,42 +230,30 @@ void Instruction::decode_opcode(string code)
 	}
 }
 
-/*
-void Instruction::decode_dest(string code)
+ int Instruction:: stringToInt(string code)
 {
-  dest = stoi(code, 0, 2);
+  int result = stoi(code, 0, 2);
+  return result;
 }
-
-void Instruction::decode_src1(string code)
-{
-  src1 = stoi(code, 0, 2);
-}
-
-void Instruction::decode_src2(string code)
-{
-  src2 = stoi(code, 0, 2);
-}
-*/
-
-
-// A class that implements the commit, execute and fetch functions, such that main()
-// simply instantiates the pipeline class and any necessary structures, and then continuously
-// calls pipeline fetch, execute and commit stages until the program has finished executing.
-class Pipeline : public Instruction
-{
-public:
-void Fetch();
-void Execute();
-void Commit();
-
-};
 
 // a class that implements the IQ. It instantiate a (finite) queue; inserts an instruction into queue;
 // check if instruction is ready for execution; printIQ() for debugging; delete an entry of the IQ during read and execute;
 class IQueue
 {
-
+public:
+  queue<string> IQueue_finite;
+  void insert_IQ_instruction(string element);
+  void delete_entry();
+  void check();
+  void printIQ();
+protected:
+  bool ready;
 };
+
+void IQueue::insert_IQ_instruction(string element)
+{
+  IQueue_finite.push(element); // NOT SURE WHAT WE'RE PUSHING TO THIS QUEUE!! STRING ELEMENT?
+}
 
 // A class that implements the IQ. It instantiate a (finite) queue; inserts an instruction into queue;
 // check if instruction is ready for execution; printIQ() for debugging; delete an entry of the IQ during read
@@ -278,6 +267,7 @@ class Execute
 class FetchUnit
 {
 
+
 };
 
 //a class that maintains the register file; setup the register file (RF) such that all register locations are associated with i) data value and ii) a status bit. The status bit indicates whether the value in the register is valid for reading or is awaiting an instruction’s result. The register # as shown in the table below is solely used to index a register. More details on the register file system is provided in a subsequent section entitled “Register File FYI”;  accessor and mutator functions should be implemented for setting or obtaining the value and status of a register.
@@ -285,14 +275,49 @@ class RegisterFile
 {
 public:
 
+};
 
+//struct to hold the data of each element in the ROB queue
+struct ROB_element
+{
+public:
+  ROB_element() : valid(), inst(), ROB_ID() {} //default constructor
+  ROB_element(bool validity, string The_inst, int The_ROB_ID) : valid(validity), inst(The_inst), ROB_ID(The_ROB_ID) {} //overlaoded constructor
+
+  bool valid; //do we want bool or just int?
+  string inst; //I have no idea what data type this should be??????
+  int ROB_ID;
 };
 
 //STL structure used to maintain a list in the pipeline, such that the instructions are inserted and retired inorder. Instructions are inserted at fetch, removed at commit when valid. An example of the contents in the ROB are shown below.
-struct ReorderBuffer //????????????????????????
+class ReorderBuffer
 {
+public:
+  ReorderBuffer();
+  //friend void access_ROB();
+  void set_ROB_element(string inst);
 
+  queue<ROB_element> ROB;
+  int ID_count;
 };
+
+/*
+ReorderBuffer::access_ROB()
+{
+  return ROB;
+}
+*/
+
+ReorderBuffer::ReorderBuffer()
+{
+  ID_count = 0;
+}
+
+void ReorderBuffer::set_ROB_element(string inst)
+{
+  ID_count++;
+  ROB.push(ROB_element(false, inst, ID_count)); //create new ROB_element in the ROB queue
+}
 
 //a class used to track and calculate latency and throughput statistics of the running program. Statistics are provided to the user via standard output at the end of a program’s execution.
 class Statistics
@@ -306,6 +331,71 @@ class HelperFunctions
 
 };
 
+// A class that implements the commit, execute and fetch functions, such that main()
+// simply instantiates the pipeline class and any necessary structures, and then continuously
+// calls pipeline fetch, execute and commit stages until the program has finished executing.
+class Pipeline : public Instruction, public ReorderBuffer, public IQueue
+{
+public:
+  Pipeline();
+  ~Pipeline();
+  void Fetch();
+  void Execute();
+  void Commit();
+  friend void set_ROB_element(string inst);
+};
+
+Pipeline::~Pipeline()
+{}
+
+Pipeline::Pipeline()
+{
+  /*
+  for (int i = 0; i < csv_contents.end(); i++)
+  {
+    uintToBinary(csv_contents[i]);
+  }
+  */
+}
+
+void Pipeline::Fetch()
+{
+  //need to check if IQ and ROB have enough free spaces to proceed to fetch more instructions
+  // if IQ (element) > 2 || ROB (element) > 2, do not fetch(?)
+  std::list<unsigned int>::iterator it;
+  it = csv_contents.begin();
+
+  while(((IQueue_finite.size() < 3) && (it != csv_contents.end())) || ((ROB.size() < 3) && (it != csv_contents.end()))) // I would like to iterate as long as it hasnt reached the end of the csv file but the while loop always goes to the end of the csv file, regardless if theres more elements in IQUEUE or ROB. This is wrong!
+  {
+    cout << "Iqueue size: " << IQueue_finite.size() << endl<< "ROB size: " << ROB.size() << endl;
+    uintToBinary(*it); // Fetch the instruction located at PC from the instruction memory
+    decode(binary_instruction); // Decode the instruction
+    set_ROB_element(binary_instruction); // Assigns a ROB ID to the instruction, places the instruction in the ROB
+    insert_IQ_instruction(binary_instruction); // and places the instruction in the IQ.
+    // CHECK THE IQ FOR VALIDY HAS NOT BEEN DONE!!!
+    // Check if the instruction is a branch or jump;
+    it++;
+  }
+  //uintToBinary(1234);
+  //decode(binary_instruction);
+  //set_ROB_element(binary_instruction);
+}
+
+void Pipeline :: Execute()
+{}
+
+void Pipeline :: Commit()
+{
+  if (ROB.front().valid == true)
+  {
+    ROB.pop();
+  }
+
+  else
+  {
+    cout << "Error: Instruction attempted to commit before valid in Reorder Buffer." << endl;
+  }
+}
 
 int main()
 {
@@ -325,8 +415,9 @@ int main()
 	Pipeline tester;
 
 	tester.read_csv("factorial.csv");
-  tester.uintToBinary(tester.get_csv_contents());
-  tester.decode(tester.binary_instructions);
+  tester.Fetch();
+  //tester.uintToBinary(tester.get_csv_contents());
+  //tester.decode(tester.binary_instructions);
 	//cout << "Hello world";
 
 }
